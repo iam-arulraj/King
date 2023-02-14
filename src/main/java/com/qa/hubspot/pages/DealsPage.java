2a635bbf9c57a23bd32b1185f4043710e9b0c194
iam-arulraj <arulraj091997@gmail.com>
2023-02-14 16:27:27 +0530

    added driver initialization part

--- src/main/java/com/qa/hubspot/pages/DealsPage.java
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -1,8 +1,12 @@
 package com.qa.hubspot.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class DealsPage {
 
-	public DealsPage() {
+	WebDriver driver;
+	public DealsPage(WebDriver driver) {
+		this.driver=driver;
 		
 	}
 }
